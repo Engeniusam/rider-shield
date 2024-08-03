5763239878432836d50ffa91df118d6933eba02a
Engeniusam <engeniusam@gmail.com>
2024-08-04 01:03:07 +0300

    Updates

---
+++ precache-manifest.ccdfb6671f09cb800d595ac9d5dbb25d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df72c64806e14b626ff50a92fae943f2",
+    "url": "/rider-shield/index.html"
+  },
+  {
+    "revision": "8f8dd2585b8747ae88f1",
+    "url": "/rider-shield/static/css/2.5f8bea38.chunk.css"
+  },
+  {
+    "revision": "cb27a4545bbe701e96e6",
+    "url": "/rider-shield/static/css/main.34dc8016.chunk.css"
+  },
+  {
+    "revision": "8f8dd2585b8747ae88f1",
+    "url": "/rider-shield/static/js/2.65931977.chunk.js"
+  },
+  {
+    "revision": "1ac23edd848720c87dd7eac90d67e14d",
+    "url": "/rider-shield/static/js/2.65931977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb27a4545bbe701e96e6",
+    "url": "/rider-shield/static/js/main.d01fa9f8.chunk.js"
+  },
+  {
+    "revision": "650008d562b273a1e1b2",
+    "url": "/rider-shield/static/js/runtime-main.a1f1f419.js"
+  },
+  {
+    "revision": "f9048895ce5498e6689b27aaf21903dd",
+    "url": "/rider-shield/static/media/bike-rider.f9048895.jpeg"
+  },
+  {
+    "revision": "6ac12761103d4257738dc31d4aec9686",
+    "url": "/rider-shield/static/media/bike-riders1.6ac12761.jpeg"
+  },
+  {
+    "revision": "3b24b570f451b4c1d50b8efb1f95bf7f",
+    "url": "/rider-shield/static/media/bki-here-for-you.3b24b570.jpeg"
+  },
+  {
+    "revision": "84c56112525bc6852606633a213855c8",
+    "url": "/rider-shield/static/media/partner1.84c56112.png"
+  },
+  {
+    "revision": "301fa0e6b37339bee430de2fe0db6173",
+    "url": "/rider-shield/static/media/partner2.301fa0e6.png"
+  },
+  {
+    "revision": "dfad0028ac17b7080722df34fa0f2842",
+    "url": "/rider-shield/static/media/partner3.dfad0028.png"
+  },
+  {
+    "revision": "23a2be97b2a40112dbf74b2a99b8eb12",
+    "url": "/rider-shield/static/media/partner4.23a2be97.svg"
+  }
+]);
